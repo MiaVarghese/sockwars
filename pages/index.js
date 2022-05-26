0cf2efcbad333a2720773d2e44795f71c1da85df
Cyue19 <yue_cheryl@hotmail.com>
2022-05-26 14:31:02 -0400

    cleaned up spacing

--- pages/index.js
+++ pages/index.js
@@ -17,31 +17,28 @@ export default function App() {
     return (
         <div>
 
-<h1 className={styles.h1}>SOCK WARS</h1>
-<h3 className={styles.h3}>Today's Immunity: Wear 2 Different Color Socks!</h3>
-<h2 className={styles.h2}>LEADERBOARD</h2>
+            <h1 className={styles.h1}>SOCK WARS</h1>
+            <h3 className={styles.h3}>Today's Immunity: Wear 2 Different Color Socks!</h3>
+            <h2 className={styles.h2}>LEADERBOARD</h2>
 
 
             <table className={styles.table}>
-
                 <tr className={styles.trh}>
                     <th className={styles.th}>Rank</th>
                     <th className={styles.th}>Player</th>
                     <th className={styles.th}>Number Eliminated</th>
                 </tr>
             
-            {APIdata.map((data) => {
-                return (
-
-                            <tr className={styles.tr}>
-                                
-                                <td className={styles.td}>{data.Rank}</td>
-                                <td className={styles.td}>{data.Player}</td>
-                                <td className={styles.td}>{data.NumberEliminated}</td>
-                            </tr>
-
-                )
-            })}
+                {APIdata.map((data) => {
+                    return (
+                        <tr className={styles.tr}>
+                            
+                            <td className={styles.td}>{data.Rank}</td>
+                            <td className={styles.td}>{data.Player}</td>
+                            <td className={styles.td}>{data.NumberEliminated}</td>
+                        </tr>
+                    )
+                })}
             </table>
 
             <table className={styles.table2}>
@@ -49,16 +46,15 @@ export default function App() {
                     <th className={styles.th}>Eliminated Players</th>
                     <th className={styles.th}>Date Eliminated</th>
                 </tr>
-            {APIdata.map((data) => {
-                return (
-
-                            <tr className={styles.tr2}>
-                                <td className={styles.td}>{data.EliminatedPlayer}</td>
-                                <td className={styles.td}>{data.EliminationDate}</td>
-                            </tr>
-                )
-            })}
-                                    </table>
+                {APIdata.map((data) => {
+                    return (
+                        <tr className={styles.tr2}>
+                            <td className={styles.td}>{data.EliminatedPlayer}</td>
+                            <td className={styles.td}>{data.EliminationDate}</td>
+                        </tr>
+                    )
+                })}
+            </table>
 
         </div>
     )
