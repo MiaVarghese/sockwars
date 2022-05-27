6b142d14d9bbb8396f99eba10f14cd15b6993b6c
Cyue19 <yue_cheryl@hotmail.com>
2022-05-27 10:17:33 -0400

    fixed login api

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,6 +1,8 @@
 import dbConnect from '../../lib/dbConnect'
 const User = require("../../models/user");
 
+import jwt from 'jsonwebtoken';
+
 export default async function handler(req, res){
     try {
         await dbConnect();
@@ -9,17 +11,15 @@ export default async function handler(req, res){
         const user = await User.findOne({ username });
         if (!user) {
             throw Error("User does not exist");
+        } else if (!(password==user.password)) {
+            throw Error("Invalid credentials");
         }
 
-        if (!password==user.password) {
-            throw Error("Invalid credentials");
-        } else {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
-            if (!token) {
-                throw Error("Couldn't sign token");
-            }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+        if (!token) {
+            throw Error("Couldn't sign token");
         }
-        
+
         res.status(200).json({token, user});
     } catch (err) {
         res.status(500).json({message: err.message});
