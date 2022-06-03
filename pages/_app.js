818e70e9331eca6ab16048b31923577aea2cd091
unknown <sja2tu@virginia.edu>
2022-06-02 21:57:44 -0400

    adding-login

--- pages/_app.js
+++ pages/_app.js
@@ -9,22 +9,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-          integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-          crossorigin="anonymous"
-        ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
       </Head>
 
-<<<<<<< HEAD
-      <Component {...pageProps} />
-    </div>
-  );
-=======
     <Component {...pageProps} />
   </div>
   )
->>>>>>> 3c7854f7722127246a1726c4be943d907c1f3da9
 }
 
 export default MyApp
