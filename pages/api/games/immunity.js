45dcc877f12274488317716dfd624bfcf547e438
Alex Yang <ay101@protonmail.com>
2022-06-27 23:02:09 -0400

    Added fetching immunity based on date

--- pages/api/games/immunity.js
+++ pages/api/games/immunity.js
@@ -7,8 +7,15 @@ export default async function handler(req, res) {
         await dbConnect();
         let game = await Game.findOne({_id:req.body.id});
         let immunities = game.immunities;
+        let currentDate = new Date();
+
+        if (currentDate >= game.startDate && currentDate <= game.endDate){
+            let immunityIndex = Math.floor((currentDate - game.startDate) / 1000 / 60 / 60 / 24)
+            res.json(immunities[immunityIndex])
+        }
         
-        res.json(immunities)
+        else
+            res.status(400).json({ message: err.message });
     }
 
     catch(err){
