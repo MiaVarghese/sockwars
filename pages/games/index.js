ad272d7046503cdd4fee9bd65e0bdb3644e32712
Cyue19 <yue_cheryl@hotmail.com>
2022-07-29 10:46:01 -0400

    games page ui

--- pages/games/index.js
+++ pages/games/index.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import axios from "axios";
 import Link from 'next/link';
 
-import styles from '../../styles/App.module.css'
+import Pagination from "../components/Pagination";
 
 const URL_PREFIX = process.env.NEXT_PUBLIC_REACT_APP_URL;
+const endPoint = process.env.NEXT_PUBLIC_REACT_APP_URL + "/auth/verify";
 
 function ListGames() {
-    const [allGames, setGames] = useState([])
+    const [allGames, setGames] = useState([]);
+    const [personalGames, setPersonalGames] = useState([]);
     const [user, setUser] = useState({})
 
     useEffect(() => {
@@ -20,32 +22,81 @@ function ListGames() {
                 console.log(typeof(response.data[i]._id))
             }
             setGames(games)
-        })
+        });
+        fetchPersonalGames();
         setUser(JSON.parse(localStorage.user))
     }, []);
 
+    async function fetchPersonalGames() {
+        try {
+            const response = await axios.get(endPoint);
+            setPersonalGames(response.data.gamesPlayed);
+            console.log(response.data.gamesPlayed);
+        } catch (err) {
+            console.log(err.response);
+        }
+    }
+
     return (
-        <>
-        <h1 style={{textAlign: "center", paddingLeft:"20px", paddingRight: "20px", color:"rgb(239, 229, 189)"}}>Games:</h1>
-        <div style={{ margin:"auto", color:"rgb(239, 229, 189)", width: "fit-content"}} class="form-group">
-        {allGames.length > 0 && (
-            allGames.map((game, idx) => {
-                return (
-                <div>
-                    <Link style={{color:"white"}} href={`/games/${encodeURIComponent(game._id)}`}>
-                        <a>Game {idx}</a>
-                    </Link> 
-                    {/* {user != null && user.role === "admin" && (
-                        <Link style={{marginLeft:"10px"}} href={`/games/${encodeURIComponent(game._id)}/immunities`}>
-                            <a>Add Immunity</a>
-                        </Link> 
-                    )} */}
+        <div className="mx-5 px-5">
+            <h1 style={{textAlign: "center", paddingLeft:"20px", paddingRight: "20px", color:"rgb(239, 229, 189)"}}>Games:</h1>
+
+            <div className="mx-5">
+                <ul className="nav nav-pills" style={{margin: "1% 3.2%"}} id="myTab" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link active" style={{borderRadius: "0"}} id="all-tab" data-bs-toggle="tab" data-bs-target="#all-tab-pane" type="button" role="tab" aria-controls="all-tab-pane" aria-selected="true">All Games</button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="personal-tab" style={{borderRadius: "0"}} data-bs-toggle="tab" data-bs-target="#personal-tab-pane" type="button" role="tab" aria-controls="personal-tab-pane" aria-selected="false">Personal</button>
+                    </li>
+                </ul>
+                <div className="tab-content" style={{margin: "0% 4%", color: "white"}} id="myTabContent">
+                    <div className="tab-pane fade show active" id="all-tab-pane" role="tabpanel" aria-labelledby="all-tab" tabindex="0">
+                        {allGames.length > 0 && (
+                            allGames.map((game, idx) => {
+                                return (
+                                <div>
+                                    <div className="row mb-1" style={{backgroundColor: "#2A3B4B"}}>
+                                        <div className="col pt-3">
+                                            <Link href={`/games/${encodeURIComponent(game._id)}`}>
+                                                <a style={{textDecoration: "none", color: "white", fontSize: "20px"}}>{game.title}</a>
+                                            </Link> 
+                                        </div>
+                                        <div className="col">
+                                            <div style={{position: "relative"}}>
+                                                <span className="text-center" style={{position: "absolute", width: "40px", paddingTop: "20px", color: "white"}}>1</span>
+                                                <i className="bi bi-star-fill me-2" style={{fontSize: "40px", color: "gold"}}></i>
+                                                <span style={{position: "absolute", top: "20px"}}>Player One</span>
+                                            </div>
+                                        </div>
+                                        <div className="col">
+                                            <div className="pt-1" style={{position: "relative"}}>
+                                                <span className="text-center" style={{position: "absolute", width: "34px", paddingTop: "15px", color: "white"}}>2</span>
+                                                <i className="bi bi-star-fill me-2" style={{fontSize: "35px", color: "gold"}}></i>
+                                                <span style={{position: "absolute", top: "20px"}}>Player Two</span>
+                                            </div>
+                                        </div>
+                                        <div className="col pt-2" style={{position: "relative"}}>
+                                            <div className="">
+                                                <span className="text-center" style={{position: "absolute", width: "30px", paddingTop: "12px", color: "white"}}>3</span>
+                                                <i className="bi bi-star-fill me-2" style={{fontSize: "30px", color: "gold"}}></i>
+                                                <span style={{position: "absolute", top: "20px"}}>Player Three</span>
+                                            </div>
+                                        </div>
+                                        <div className="col pt-3 px-0">{new Date(game.startDate).toLocaleString()}</div>
+                                        <div className="col-2 pt-3 text-center px-0">{game.status}</div>
+                                    </div>
+                                </div>
+                                )
+                            })
+                        )}
+
+                        
+                    </div>
+                    <div className="tab-pane fade" id="personal-tab-pane" role="tabpanel" aria-labelledby="personal-tab" tabindex="0">personal</div>
                 </div>
-                )
-            })
-        )}
+            </div>
         </div>
-        </>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import styles from "../styles/App.module.css";
-
-// export default function App() {
-//   const [APIdata, setAPIdata] = useState([]);
-
-//   useEffect(() => {
-//     axios
-//       .get("https://sheet.best/api/sheets/fe557b31-4e38-489c-81db-d048fb75d6d8")
-//       .then((incomingData) => {
-//         setAPIdata(incomingData.data);
-//       })
-//       .catch((err)=> {
-//         console.log(err.response);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1 className={styles.h1}>SOCK WARS</h1>
-//       <h3 className={styles.h3}>
-//         Today's Immunity: Wear 2 Different Color Socks!
-//       </h3>
-//       <h2 className={styles.h2}>LEADERBOARD</h2>
-
-//       <table className={styles.table}>
-//         <tr className={styles.trh}>
-//           <th className={styles.th}>Rank</th>
-//           <th className={styles.th}>Player</th>
-//           <th className={styles.th}>Number Eliminated</th>
-//         </tr>
-
-//         {APIdata.map((data) => {
-//           return (
-//             <tr className={styles.tr}>
-//               <td className={styles.td}>{data.Rank}</td>
-//               <td className={styles.td}>{data.Player}</td>
-//               <td className={styles.td}>{data.NumberEliminated}</td>
-//             </tr>
-//           );
-//         })}
-//       </table>
-
-//       <table className={styles.table2}>
-//         <tr className={styles.trh2}>
-//           <th className={styles.th}>Eliminated Players</th>
-//           <th className={styles.th}>Date Eliminated</th>
-//         </tr>
-//         {APIdata.map((data) => {
-//           return (
-//             <tr className={styles.tr2}>
-//               <td className={styles.td}>{data.EliminatedPlayer}</td>
-//               <td className={styles.td}>{data.EliminationDate}</td>
-//             </tr>
-//           );
-//         })}
-//       </table>
-//     </div>
-//   );
-// }
-
 import styles from "../styles/confirmelim.module.css";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
@@ -70,40 +8,43 @@ const endPoint = process.env.NEXT_PUBLIC_REACT_APP_URL + "/games/";
 
 export default function Gamehistory() {
   const [activePlyrs, setActivePlyrs] = useState();
-  const { currGame } = useContext(GameContext);
+  const [game, setGame] = useState();
+  const { prevGame, currGame } = useContext(GameContext);
 
   const router = useRouter();
 
   useEffect(() => {
-    try {
-      fetchGamehistory();
-    } catch (err) {
-      console.log(err);
+    if (currGame) {
+      setGame(currGame);
+    } else if (prevGame) {
+      setGame(prevGame);
     }
-  }, [currGame]);
 
-  async function fetchGamehistory() {
-    try {
-      const response = await axios.get(endPoint);
-      setActivePlyrs(response.data.activePlayers);
-      console.log(response.data.activePlayers);
-    } catch (err) {
-      console.log(err);
-      if (err.response) {
-        console.log(err.response);
-      }
-    }
-  }
+    // try {
+    //   fetchGamehistory();
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  }, [prevGame, currGame]);
+
+  // async function fetchGamehistory() {
+  //   try {
+  //     const response = await axios.get(endPoint);
+  //     setActivePlyrs(response.data.activePlayers);
+  //     console.log(response.data.activePlayers);
+  //   } catch (err) {
+  //     console.log(err);
+  //     if (err.response) {
+  //       console.log(err.response);
+  //     }
+  //   }
+  // }
 
   return (
     <div>
-      {currGame ? (
+      {game ? (
         <div>
           <title>Leaderboard </title>
-          <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-          ></link>
           <div className={styles.n}>
             <p>SOCK WARS</p>
           </div>
@@ -113,7 +54,7 @@ export default function Gamehistory() {
           </div>
           <div className={styles.box}>
             Game #:
-            <h3>{currGame._id}</h3>
+            <h3>{game.title}</h3>
             <div className={styles.chart}>
               <table class="table table-striped">
                 <thead>
@@ -124,7 +65,7 @@ export default function Gamehistory() {
                   </tr>
                 </thead>
                 <tbody>
-                  {currGame.activePlayers.map((activePlyr, index) => (
+                  {game.activePlayers.map((activePlyr, index) => (
                     <tr>
                       <td>
                         <div>{index + 1}</div>
@@ -147,7 +88,7 @@ export default function Gamehistory() {
           </div>
           <div className={styles.box}>
             Game #:
-            <h3>{currGame._id}</h3>
+            <h3>{game._id}</h3>
             {/* <h3>{gamehistory.eliminatedPlayers.username}</h3> */}
             <div className={styles.chart}>
               <table class="table table-striped">
@@ -159,7 +100,7 @@ export default function Gamehistory() {
                   </tr>
                 </thead>
                 <tbody>
-                  {currGame.eliminatedPlayers.map((eliminatedPlyr) => (
+                  {game.eliminatedPlayers.map((eliminatedPlyr) => (
                     <tr>
                       <td>
                         <div>{eliminatedPlyr.username}</div>
